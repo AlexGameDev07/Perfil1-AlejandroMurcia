bc3e98ad25bcc63bdb5f2cdd9a56f2dd561e3e4f
Alex_Dev <alekeymurcia@gmail.com>
2024-02-10 18:33:02 -0600

    ver 1.9

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -1,7 +1,22 @@
-function scrollToContent(Id_Location) {
-    
-    var contentElement = document.getElementById(Id_Location)
-  
-    
-    contentElement.scrollIntoView({ behavior: 'smooth', block: 'start' })
+function scrollToContent(event) {
+    // Evitar que el enlace recargue la página
+    event.preventDefault();
+
+    // Obtener el ID del acordeón desde el atributo data-target del enlace
+    var accordionId = event.target.getAttribute('data-bs-target');
+
+    // Obtener el elemento del acordeón
+    var accordionElement = document.querySelector(accordionId);
+
+    // Abrir el acordeón
+    $(accordionElement).collapse('show');
+
+    // Calcular la posición del acordeón en la página
+    var accordionRect = accordionElement.getBoundingClientRect();
+    var scrollPosition = accordionRect.top + window.pageYOffset - (window.innerHeight / 2);
+
+    // Desplazar la página para que el acordeón quede centrado verticalmente
+    window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+
+
 }
