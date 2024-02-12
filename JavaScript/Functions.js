64937ebbe55f91fd0542aca303841753db90a4bd
Alex_Dev <alekeymurcia@gmail.com>
2024-02-11 20:29:33 -0600

    ver 1.9.2

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -1,22 +1,10 @@
-function scrollToContent(event) {
-    // Evitar que el enlace recargue la página
-    event.preventDefault();
+// function scrollToContent(N_Emprededor) {
 
-    // Obtener el ID del acordeón desde el atributo data-target del enlace
-    var accordionId = event.target.getAttribute('data-bs-target');
+//     let Id_Accordion = ["panelsStayOpen-collapseOne", "panelsStayOpen-collapseTwo", "panelsStayOpen-collapseThree", "panelsStayOpen-collapseFour", "panelsStayOpen-collapseFive", "panelsStayOpen-collapseSix", "panelsStayOpen-collapseSeven", "panelsStayOpen-collapseEight", "panelsStayOpen-collapseNine", "panelsStayOpen-collapseTen"]
+    
+//     var accordionElement = document.getElementById(Id_Accordion[N_Emprededor-1])
 
-    // Obtener el elemento del acordeón
-    var accordionElement = document.querySelector(accordionId);
-
-    // Abrir el acordeón
-    $(accordionElement).collapse('show');
-
-    // Calcular la posición del acordeón en la página
-    var accordionRect = accordionElement.getBoundingClientRect();
-    var scrollPosition = accordionRect.top + window.pageYOffset - (window.innerHeight / 2);
-
-    // Desplazar la página para que el acordeón quede centrado verticalmente
-    window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+//     window.scrollTo()
 
 
-}
+// }
